b4ca0be8cb3baecea6c3dc22adf959fa176d280e
Theodor Vararu <theo@vararu.org>
2022-10-12 10:34:11 +0300

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
   root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   scope via: :all do
     get "/404", to: "errors#not_found"
