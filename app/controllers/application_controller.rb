bef6d0e741afa3fd34d7f47ea53b25998d89f5f4
Theodor Vararu <theo@vararu.org>
2022-10-12 10:25:38 +0300

    Add basic auth in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
+  if Rails.env.production?
+    http_basic_authenticate_with name: "design",
+                                 password: "history",
+                                 message: "THIS IS NOT A REAL GOV.UK SERVICE"
+  end
+
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
 end
