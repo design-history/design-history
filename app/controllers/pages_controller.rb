c41681e106daf031564b3310a79e1d15f619b023
Theodor Vararu <theo@vararu.org>
2022-10-14 11:44:51 +0200

    Redirect home to posts index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,2 @@
 class PagesController < ApplicationController
-  def home
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "pages#home"
+  root to: redirect("/posts")
 
   resources :posts
 
