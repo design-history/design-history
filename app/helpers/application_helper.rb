90b8a742ea069800f417357a0ef038dc0b28614f
Theodor Vararu <theo@vararu.org>
2022-10-15 12:48:12 +0200

    Add govuk_row and govuk_two_thirds helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def govuk_row(&block)
+    tag.div(class: "govuk-grid-row", &block)
+  end
+
+  def govuk_two_thirds(&block)
+    tag.div(class: "govuk-grid-column-two-thirds-from-desktop", &block)
+  end
 end
