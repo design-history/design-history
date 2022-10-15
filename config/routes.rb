1233831afe7b3fb482307945440812b8b2961f7e
Theodor Vararu <theo@vararu.org>
2022-10-15 11:45:56 +0200

    Add projects_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: redirect("/posts")
 
-  resources :posts
+  resources :projects do
+    resources :posts
+  end
 
   scope via: :all do
     get "/404", to: "errors#not_found"
