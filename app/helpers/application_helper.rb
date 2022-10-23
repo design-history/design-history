2532bc21102cdbbc2dfe06201f990dedf0c91560
Theodor Vararu <theo@vararu.org>
2022-10-23 17:31:22 +0300

    Add govuk_one_third helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     tag.div(class: "govuk-grid-row", &block)
   end
 
+  def govuk_one_third(&block)
+    tag.div(class: "govuk-grid-column-one-third-from-desktop", &block)
+  end
+
   def govuk_two_thirds(&block)
     tag.div(class: "govuk-grid-column-two-thirds-from-desktop", &block)
   end
