291d82f52bc6a4ac8a70938221e2041f9d077ddd
Theodor Vararu <theo@vararu.org>
2022-10-26 00:19:44 +0300

    Don't ask for user on posts index/show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: %i[index show]
   before_action :set_project
   before_action :set_post, only: %i[show edit update destroy]
 
