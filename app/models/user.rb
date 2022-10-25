8cbac48f81a87ada5c498d9c430b8e1ad4d01a49
Theodor Vararu <theo@vararu.org>
2022-10-25 11:29:54 +0300

    Switch on devise :trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :timeoutable, and :omniauthable
   devise :database_authenticatable,
-         :registerable,
          :recoverable,
+         :registerable,
          :rememberable,
+         :trackable,
          :validatable
 end
