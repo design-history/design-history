06e8e92720bdb1538c657ead7919bbca72b657d6
Theodor Vararu <theo@vararu.org>
2022-10-27 17:01:58 +0300

    Add validations to project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -20,4 +20,7 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :posts, dependent: :destroy
+
+  validates :title, presence: true, length: { maximum: 255 }
+  validates :description, presence: true, length: { maximum: 255 }
 end
