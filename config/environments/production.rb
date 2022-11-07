b7c371c1929b130bc7c903c2a77825b439ad1d58
Theodor Vararu <theo@vararu.org>
2022-11-07 22:08:20 +0000

    Add designhistory.io to production hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,6 +79,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.hosts << "designhistory.io"
   config.hosts << "designhistory.app"
   config.app_domain = "designhistory.app"
 
