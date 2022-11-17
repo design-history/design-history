f60004476c5d07f0c1d010e05f1621ef912fdbd0
Theodor Vararu <theo@vararu.org>
2022-11-17 00:50:51 +0000

    Add PostImagesController
    This will be used to isolate logic related to uploading, destroying, and
    moving post images.

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,51 @@
+class PostImagesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_project
+  before_action :set_post
+  before_action :set_image, except: [:create]
+
+  def up
+    @post.ordered_image_move_up!(@image)
+    redirect_to edit_project_post_path(@project, @post),
+                notice: "Image was successfully moved"
+  end
+
+  def down
+    @post.ordered_image_move_down!(@image)
+    redirect_to edit_project_post_path(@project, @post),
+                notice: "Image was successfully moved"
+  end
+
+  def create
+    if @post.update(create_params)
+      redirect_to edit_project_post_path(@project, @post),
+                  notice: "Images were successfully uploaded"
+    else
+      render "post/edit", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @image.purge
+    redirect_to edit_project_post_path(@project, @post),
+                notice: "Image was successfully deleted"
+  end
+
+  private
+
+  def create_params
+    params.require(:post).permit(images: [])
+  end
+
+  def set_project
+    @project = current_user.projects.find(params[:project_id])
+  end
+
+  def set_post
+    @post = @project.posts.with_attached_images.find(params[:post_id])
+  end
+
+  def set_image
+    @image = @post.images.find(params[:id])
+  end
+end
