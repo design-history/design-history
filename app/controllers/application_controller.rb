ce39bb1ff4aa991194164aad5253886098969155
Theodor Vararu <theo@vararu.org>
2022-11-22 19:21:15 +0000

    Remove basic auth
    Showing a thing soon :tada:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  if Rails.env.production?
-    http_basic_authenticate_with name: "design",
-                                 password: "history",
-                                 message: "THIS IS NOT A REAL GOV.UK SERVICE"
-  end
-
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
 end
