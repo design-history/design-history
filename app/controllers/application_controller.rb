f723df9c04dfbb171bcca37de6c0b9fda7848bca
Theodor Vararu <theo@vararu.org>
2022-11-22 21:47:09 +0000

    Redirect users to projects page post sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
+
+  def after_sign_in_path_for(_)
+    projects_path
+  end
 end
