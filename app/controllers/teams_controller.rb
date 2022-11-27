01606fc320624c21103403d0d0fbd79da8ca98c4
Theodor Vararu <theo@vararu.org>
2022-11-27 16:09:06 +0000

    Add team#create route

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -8,11 +8,17 @@ class TeamsController < ApplicationController
 
   # POST /teams
   def create
-    # @project = current_user.projects.new(project_params)
-    # if @project.save
-    #   redirect_to @project, notice: "Your design history has been created"
-    # else
-    #   render :new, status: :unprocessable_entity
-    # end
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to @team, notice: "Your team has been created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:name)
   end
 end
