391090bd9b2e18e7dec644d29e3dd8180098c87a
Theodor Vararu <theo@vararu.org>
2022-11-27 22:48:24 +0000

    Add add user form
    Allows users to add users to their teams.

---
+++ app/forms/add_user_form.rb
@@ -0,0 +1,25 @@
+class AddUserForm
+  include ActiveModel::Model
+
+  attr_accessor :email, :team
+  validates :email, presence: true
+  validates :team, presence: true
+
+  def save
+    return false if invalid?
+
+    user = User.find_by_email(email)
+
+    unless user
+      errors.add(:email, :not_found)
+      return false
+    end
+
+    if user.team.present?
+      errors.add(:email, :has_team)
+      return false
+    end
+
+    true
+  end
+end
