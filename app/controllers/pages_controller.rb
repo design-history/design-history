c330af7fab9f9aa37c9e26a813baca8fad2c0844
Theodor Vararu <theo@vararu.org>
2022-12-01 21:09:02 +0000

    Add robots.txt route
    It blocks attempts to crawl on non-production environments, as well as
    on private projects.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,12 @@ class PagesController < ApplicationController
   def landing
     render layout: "landing"
   end
+
+  def robots
+    project = Project.find_by(subdomain: request.subdomain)
+    @block_crawlers = !Rails.env.production? || project&.private?
+
+    respond_to :text
+    expires_in 6.hours, public: true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get "/robots.:format", to: "pages#robots"
+
   scope via: :all do
     get "/404", to: "errors#not_found"
     get "/422", to: "errors#unprocessable_entity"
