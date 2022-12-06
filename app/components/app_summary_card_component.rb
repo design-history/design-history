b736f6b8be8307163e872108b162a9635d582978
Theodor Vararu <theo@vararu.org>
2022-12-06 20:08:37 +0000

    Add AppSummaryCardComponent
    Based on the XGovukSummaryCardComponent.

---
+++ app/components/app_summary_card_component.rb
@@ -0,0 +1,30 @@
+class AppSummaryCardComponent < ViewComponent::Base
+  def initialize(rows:, border: true, editable: true, ignore_editable: [])
+    super
+    rows = transform_hash(rows) if rows.is_a?(Hash)
+    @rows = rows_including_actions_if_editable(rows, editable, ignore_editable)
+    @border = border
+  end
+
+  def border_css_class
+    @border ? "" : "no-border"
+  end
+
+  private
+
+  attr_reader :rows, :ignore_editable
+
+  def rows_including_actions_if_editable(rows, editable, ignore_editable)
+    rows.map do |row|
+      row.tap do |r|
+        next if r[:key].in? ignore_editable
+
+        r.delete(:action) unless editable
+      end
+    end
+  end
+
+  def transform_hash(row_hash)
+    row_hash.map { |key, value| { key:, value: } }
+  end
+end
---
+++ app/components/app_summary_card_header_component.rb
@@ -0,0 +1,8 @@
+class AppSummaryCardHeaderComponent < ViewComponent::Base
+  def initialize(title:, heading_level: 2, anchor: nil)
+    super
+    @title = title
+    @heading_level = heading_level
+    @anchor = anchor
+  end
+end
