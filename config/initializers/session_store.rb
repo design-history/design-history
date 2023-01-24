8baf90faa4c98904cfec26936b38b0e39bbc720d
Paul Hayes <fofr@users.noreply.github.com>
2023-01-24 21:42:24 +0000

    Increase cookie length to 1 year

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 Rails.application.config.session_store :cookie_store,
                                        secure: Rails.env.production?,
-                                       expire_after: 2.hours
+                                       expire_after: 1.year
