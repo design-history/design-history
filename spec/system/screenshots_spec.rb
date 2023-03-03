0984fd083c93c6559a4c6c9f7d3597e3693d2f3b
Theodor Vararu <theo@vararu.org>
2023-03-03 23:12:33 +0000

    Add screenshots_spec
    This tests the attaching, reordering, and deletion functionality on
    screenshots.

---
+++ spec/system/screenshots_spec.rb
@@ -0,0 +1,70 @@
+require "rails_helper"
+
+RSpec.describe "Screenshots" do
+  it "can be attached, deleted, reordered" do
+    given_i_am_signed_in
+    when_i_visit_my_project
+    then_i_see_the_screenshots_section
+
+    when_i_upload_an_image
+    then_i_see_the_image
+
+    when_i_upload_another_image
+    then_i_see_both_images
+
+    when_i_move_the_first_image_down
+    and_i_delete_the_second_image
+    then_i_see_the_first_image
+  end
+
+  private
+
+  def given_i_am_signed_in
+    @owner = create(:user)
+    sign_in @owner
+  end
+
+  def when_i_visit_my_project
+    @project = create(:project, owner: @owner)
+    @post = create(:post, project: @project)
+    visit edit_project_post_path(@project, @post)
+  end
+
+  def then_i_see_the_screenshots_section
+    expect(page).to have_content "Images"
+  end
+
+  def when_i_upload_an_image
+    attach_file "post[images][]",
+                Rails.root.join("spec/fixtures/files/screenshot.png")
+    click_button "Save images"
+  end
+
+  def then_i_see_the_image
+    expect(page).to have_css "img[src*='screenshot.png']"
+  end
+
+  def when_i_upload_another_image
+    attach_file "post[images][]",
+                Rails.root.join("spec/fixtures/files/screenshot2.png")
+    click_button "Save images"
+  end
+
+  def then_i_see_both_images
+    expect(page).to have_css "img[src*='screenshot.png']"
+    expect(page).to have_css "img[src*='screenshot2.png']"
+  end
+
+  def when_i_move_the_first_image_down
+    first("button", text: "Move down image").click
+  end
+
+  def and_i_delete_the_second_image
+    first("button", text: "Delete image").click
+  end
+
+  def then_i_see_the_first_image
+    expect(page).to have_css "img[src*='screenshot.png']"
+    expect(page).to have_no_css "img[src*='screenshot2.png']"
+  end
+end
