edc103213bedaad8901d9ee485a76c1ccc80c43b
Theodor Vararu <theo@vararu.org>
2023-03-09 19:02:20 +0100

    Add admin field to users
    This will allow us to distinguish between regular users and admins. We
    can flip it on/off using the Rails console initially.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_25_131201) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_09_180122) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -106,6 +106,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_25_131201) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "team_id"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["team_id"], name: "index_users_on_team_id"
