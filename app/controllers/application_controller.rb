0cc5ef26e0ce1ecf4f8ca2e0592475a18b42810d
Theodor Vararu <theo@vararu.org>
2023-03-19 20:41:27 +0100

    Add markdown embed code to screenshots page
    This should allow users to more easily embed images inline into their
    posts.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :set_active_storage_url_options
+
   default_form_builder(GOVUKDesignSystemFormBuilder::FormBuilder)
 
   def after_sign_in_path_for(_)
     projects_path
+  end
+
+  private
+
+  def set_active_storage_url_options
+    ActiveStorage::Current.url_options = { host: request.base_url }
   end
 end
