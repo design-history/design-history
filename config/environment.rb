78ed220261f6d20b6faed09b94785a9b47546516
Theodor Vararu <theo@vararu.org>
2023-05-22 14:52:41 +0100

    Add Mailgun SMTP settings
    Updates the Rails encrypted secrets with the Mailgun API key and
    configures the app to use Mailgun for transactional emails.
    
    With this, the "Forgot your password" flow should start working.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: "apikey", # This is the string literal 'apikey', NOT the ID of your API key
+  password: Rails.application.credentials.dig(:mailgun, :api_key),
+  domain: "yourdomain.com",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
