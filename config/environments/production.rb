09f54f520ac1ba123b748c7b3cb5bce75b014a33
Theodor Vararu <theo@vararu.org>
2023-05-22 15:04:53 +0100

    Configure email host in production
    Forgot to do this in the previous PR.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: "designhistory.io" }
 
   config.admin_domain = "designhistory.io"
   config.hosts << "designhistory.io"
