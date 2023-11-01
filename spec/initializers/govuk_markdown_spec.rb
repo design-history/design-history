4b964d19a922147336ec5b2b07e91b3a08ffc17c
Theodor Vararu <theo@vararu.org>
2023-11-01 08:01:03 +0000

    Add unit test for GovukMarkdown extensions

---
+++ spec/initializers/govuk_markdown_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+describe 'GovukMarkdownExtension' do
+  it 'renders smart quotes' do
+    markdown = "I'm a smart quote"
+    rendered_text = GovukMarkdown.render(markdown)
+    expect(rendered_text).to include('&rsquo;')
+  end
+
+  it 'renders fenced code blocks' do
+    markdown = "```\ncode block\n```"
+    rendered_text = GovukMarkdown.render(markdown)
+    expect(rendered_text).to include('<pre>')
+  end
+
+  it 'renders highlights' do
+    markdown = "This is ==highlighted=="
+    rendered_text = GovukMarkdown.render(markdown)
+    expect(rendered_text).to include('<mark>')
+  end
+
+  it 'renders emails' do
+    markdown = <<~MD
+      {email}
+      Subject: This is a subject
+      To: foo@example.com
+      ---
+      # This is the body
+
+      With some markdown, and a ((placeholder))
+      {/email}
+    MD
+    rendered_text = GovukMarkdown.render(markdown)
+    expect(rendered_text).to include('<div class="app-email">')
+    expect(rendered_text).to include('<h1')
+    expect(rendered_text).to include('<mark>placeholder</mark>')
+  end
+
+  it "doesn't render emails when they're in a code block" do
+    markdown = <<~MD
+      ```
+      {email}
+      Subject: This is a subject
+      ---
+      # This is the body
+      {/email}
+      ```
+    MD
+    rendered_text = GovukMarkdown.render(markdown)
+    expect(rendered_text).to include('<pre>')
+    expect(rendered_text).not_to include('<div class="app-email">')
+  end
+end
