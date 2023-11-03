396bae9036a008c5c7fc139d7ccecbb6f3cda986
Theodor Vararu <theo@vararu.org>
2023-11-03 11:27:50 +0000

    Migrate database to 7.1 defaults
    Rails 7.1 update introduced 3 new migrations.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: comments
+#
+#  id               :bigint           not null, primary key
+#  body             :text             not null
+#  commentable_type :string           not null
+#  email            :string           not null
+#  name             :string           not null
+#  spam             :boolean          default(FALSE)
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  commentable_id   :bigint           not null
+#
+# Indexes
+#
+#  index_comments_on_commentable  (commentable_type,commentable_id)
+#
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
