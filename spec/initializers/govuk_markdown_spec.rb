d5f691fa3a778c020f125f719abd9a94c5a2565a
Theodor Vararu <theo@vararu.org>
2023-11-05 09:29:06 +0000

    Make headers optional in email markdown
    If there are no headers specified, then the whole content of the email
    is assumed to be the body.

--- spec/initializers/govuk_markdown_spec.rb
+++ spec/initializers/govuk_markdown_spec.rb
@@ -19,21 +19,39 @@ describe 'GovukMarkdownExtension' do
     expect(rendered_text).to include('<mark>')
   end
 
-  it 'renders emails' do
-    markdown = <<~MD
-      {email}
-      Subject: This is a subject
-      To: foo@example.com
-      ---
-      # This is the body
+  describe 'emails' do
+    it 'render correctly' do
+      markdown = <<~MD
+        {email}
+        Subject: This is a subject
+        To: foo@example.com
+        ---
+        # This is the body
 
-      With some markdown, and a ((placeholder))
-      {/email}
-    MD
-    rendered_text = GovukMarkdown.render(markdown)
-    expect(rendered_text).to include('<div class="app-email">')
-    expect(rendered_text).to include('<h1')
-    expect(rendered_text).to include('<mark>placeholder</mark>')
+        With some markdown, and a ((placeholder))
+        {/email}
+      MD
+      rendered_text = GovukMarkdown.render(markdown)
+      expect(rendered_text).to include('<div class="app-email">')
+      expect(rendered_text).to include('<dl class="app-email-headers">')
+      expect(rendered_text).to include('<h1')
+      expect(rendered_text).to include('<mark>placeholder</mark>')
+    end
+
+    it 'render correctly when header is missing' do
+      markdown = <<~MD
+        {email}
+        # This is the body
+
+        With some markdown, and a ((placeholder))
+        {/email}
+      MD
+      rendered_text = GovukMarkdown.render(markdown)
+      expect(rendered_text).to include('<div class="app-email">')
+      expect(rendered_text).not_to include('<dl class="app-email-headers">')
+      expect(rendered_text).to include('<h1')
+      expect(rendered_text).to include('<mark>placeholder</mark>')
+    end
   end
 
   it "doesn't render emails when they're in a code block" do
