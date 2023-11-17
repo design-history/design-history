8f1dc4a048c4f840738d93dd44a771283810ae9a
Theodor Vararu <theo@vararu.org>
2023-11-17 11:26:54 +0100

    Add h1 and page_title helpers
    The page_title helper forces us to declare a title on every page, and
    also automatically prefixes error pages with "Error:". Used correctly,
    it should make sure that we're using <h1> tags consistently and that
    error pages return a 422 status code.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,33 @@
 module ApplicationHelper
+  def h1(text = nil, **options, &block)
+    title_text = options[:page_title] || text
+    options.delete(:page_title)
+
+    content_for(:page_title, title_text) unless content_for?(:page_title)
+
+    if block_given?
+      if title_text.blank?
+        raise ArgumentError, "Must provide title option when using block"
+      end
+      content_tag(:h1, options, &block)
+    else
+      content_tag(:h1, text, **options)
+    end
+  end
+
+  def page_title(service_name)
+    title = content_for(:page_title)
+
+    if title.blank?
+      raise "No page title set. Either use the <%= h1 %> helper in your page, \
+or set it with content_for(:page_title)."
+    end
+
+    title = "Error: #{title}" if response.status == 422
+
+    [title, service_name].join(" - ")
+  end
+
   def govuk_row(&block)
     tag.div(class: "govuk-grid-row", &block)
   end
