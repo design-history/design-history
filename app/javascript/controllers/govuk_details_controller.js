5924747e0aa4c0872754e2eb4dd12a0f46bb8e41
Theodor Vararu <theo@vararu.org>
2023-12-11 08:57:44 +0000

    Remove references to govuk-details JS
    This has been removed in v5.

--- app/javascript/controllers/govuk_details_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "@hotwired/stimulus";
-import { Details } from "govuk-frontend";
-
-// Connects to data-module="govuk-details"
-export default class extends Controller {
-  connect() {
-    new Details(this.element).init();
-  }
-}
