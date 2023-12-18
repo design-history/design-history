cd5c1b3f7a072ff58f72a1a0f07f0b6beecdccdf
Theodor Vararu <theo@vararu.org>
2023-12-18 08:05:33 +0000

    Bump schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_12_143210) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_19_203028) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
