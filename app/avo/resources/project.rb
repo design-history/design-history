077b15efad9ebf343ccb95cecbc031cd7e3ab470
Theodor Vararu <theo@vararu.org>
2023-12-18 08:12:36 +0000

    Migrate avo resources to v3 syntax

---
+++ app/avo/resources/project.rb
@@ -0,0 +1,23 @@
+class Avo::Resources::Project < Avo::BaseResource
+  self.title = :title
+  self.includes = []
+  # self.search_query = -> do
+  #   query.ransack(id_eq: params[:q], m: "or").result(distinct: false)
+  # end
+
+  def fields
+    field :id, as: :id
+    # Fields generated from the model
+    field :title, as: :text
+    # field :description, as: :textarea
+    field :subdomain, as: :text
+    # field :password, as: :password
+    field :owner_type, as: :text
+    field :owner_id, as: :number
+    field :theme, as: :text
+    # field :related_links, as: :textarea
+    # field :owner, as: :belongs_to
+    # field :posts, as: :has_many
+    # add fields here
+  end
+end
