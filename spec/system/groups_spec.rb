7b54d0dc75862522cb63490eae8ecf4c74c09bb4
Theodor Vararu <theo@vararu.org>
2024-02-03 12:31:49 -0400

    WIP groups spec

--- spec/system/groups_spec.rb
+++ spec/system/groups_spec.rb
@@ -8,6 +8,21 @@ RSpec.describe "Groups" do
 
     when_i_click_the_new_group_button
     then_i_see_the_new_group_form
+
+    when_i_fill_in_and_submit_the_form
+    then_i_see_the_group_page
+
+    when_i_edit_the_group
+    then_i_see_the_edit_group_form
+
+    when_i_change_the_title_and_submit_the_form
+    then_i_see_the_updated_group_page
+
+    when_i_click_the_add_project_link
+    then_i_see_the_add_project_form
+
+    when_i_choose_a_project_and_submit_the_form
+    then_i_see_the_project_on_the_group_page
   end
 
   private
@@ -36,5 +51,51 @@ RSpec.describe "Groups" do
 
   def then_i_see_the_new_group_form
     expect(page).to have_content "New group"
+  end
+
+  def when_i_fill_in_and_submit_the_form
+    fill_in "Title", with: "Becoming a juggler desing (typo) history"
+    fill_in "Group subdomain", with: "this"
+    fill_in "Description",
+      with: "A history of the designs for the Becoming a juggler team"
+    click_button "Create group"
+  end
+
+  def then_i_see_the_group_page
+    expect(page).to have_content "Becoming a juggler desing (typo) history"
+  end
+
+  def when_i_edit_the_group
+    click_link "Change details"
+  end
+
+  def then_i_see_the_edit_group_form
+    expect(page).to have_content "Change details"
+  end
+
+  def when_i_change_the_title_and_submit_the_form
+    fill_in "Title", with: "Becoming a juggler design history"
+    click_button "Save changes"
+  end
+
+  def then_i_see_the_updated_group_page
+    expect(page).to have_content "Becoming a juggler design history"
+  end
+
+  def when_i_click_the_add_project_link
+    click_link "Add design history"
+  end
+
+  def then_i_see_the_add_project_form
+    expect(page).to have_content "Add design history"
+  end
+
+  def when_i_choose_a_project_and_submit_the_form
+    choose @project1.title, visible: false
+    click_button "Save changes"
+  end
+
+  def then_i_see_the_project_on_the_group_page
+    expect(page).to have_content @project1.title
   end
 end
