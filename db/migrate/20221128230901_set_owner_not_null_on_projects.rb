eb0c10134d23a7c3157a1d40d61b80e41a19c26b
Theodor Vararu <theo@vararu.org>
2024-07-15 09:41:43 +0200

    Fix migration lint warning

--- db/migrate/20221128230901_set_owner_not_null_on_projects.rb
+++ db/migrate/20221128230901_set_owner_not_null_on_projects.rb
@@ -1,6 +1,15 @@
 class SetOwnerNotNullOnProjects < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null :projects, :owner_id, false
-    change_column_null :projects, :owner_type, false
+  def up
+    change_table :projects, bulk: true do |t|
+      t.change :owner_id, :bigint, null: false
+      t.change :owner_type, :string, null: false
+    end
+  end
+
+  def down
+    change_table :projects, bulk: true do |t|
+      t.change :owner_id, :bigint, null: true
+      t.change :owner_type, :string, null: true
+    end
   end
 end
